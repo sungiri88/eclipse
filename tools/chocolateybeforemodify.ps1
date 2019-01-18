6b7d51e1262fe0b2745c866668306abf7361bd37
ugirirajan <ugirirajan@CORP.AMAZONWORKSPACES.COM>
2019-01-18 09:27:50 +0530

    first commit

---
+++ tools/chocolateybeforemodify.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+. "$toolsDir\Uninstall-ChocolateyShortcut.ps1"
+ 
+$packageVersion = $Env:ChocolateyPackageVersion
+ 
+$shortcutName = "Eclipse $packageVersion.lnk"
+$shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonPrograms)) $shortcutName
+if (Test-Path $shortcutPath) {
+    Uninstall-ChocolateyShortcut $shortcutPath -UnpinFromTaskbar
+}
+$shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonDesktopDirectory)) $shortcutName
+if (Test-Path $shortcutPath) {
+    Uninstall-ChocolateyShortcut $shortcutPath
+}
+ 
+$logPath = Join-Path $Env:ChocolateyPackageFolder "eclipse.$packageVersion.txt"
+$installationPath = Get-Content $logPath
+Write-Verbose "Previous Installation Path: $installationPath"
+Remove-Item -Path $logPath -ErrorAction SilentlyContinue
+ 
+Remove-Item -Path $installationPath -ErrorAction SilentlyContinue -Recurse -Force
+ 
+$logPath = Join-Path $Env:ChocolateyPackageFolder '*.zip.txt'
+Remove-Item -Path $logPath -ErrorAction SilentlyContinue
---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,65 @@
+﻿$ErrorActionPreference = 'Stop'
+ 
+$toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
+. "$toolsDir\Get-PackageParameters.ps1"
+$packageVersion = $Env:ChocolateyPackageVersion
+$packageParameters = Get-PackageParameters
+$filename='eclipse-jee-2018-12-R-win32-x86_64.zip'
+if ($toolsDir.Contains('/')) { $url = "$toolsDir/$filename" }
+else { $url = "$toolsDir\$filename" }
+ 
+if ($packageParameters.ContainsKey('InstallationPath')) {
+    $installationPath = Join-Path $packageParameters.Item('InstallationPath') $packageVersion
+} elseif ($packageParameters.ContainsKey('InstallLocation')) {
+    $installationPath = Join-Path $packageParameters.Item('InstallLocation') $packageVersion
+} else {
+    $forceX86 = if ($Env:ChocolateyForceX86) { Get-ProcessorBits 64 } else { $false }
+    $programFilesPath = if ($forceX86) { ${Env:ProgramFiles(x86)} } else { $Env:ProgramFiles }
+    $installationPath = Join-Path $programFilesPath "Eclipse $packageVersion"
+}
+Write-Verbose "Installation Path: $installationPath"
+ 
+$multiUser = $packageParameters.ContainsKey('Multi-User')
+ 
+# *** Automatically filled ***
+$packageArgs = @{
+    packageName    = 'eclipse'
+    url            = $url
+    url64bit       = $url
+    unzipLocation  = $installationPath
+    checksum       = 'ebdc7f0a56b2bcaaf3a57c32d7e56d690e7579f7cc15d3d2daff5bea28208810'
+    checksumType   = 'sha256'
+    checksum64     = 'ebdc7f0a56b2bcaaf3a57c32d7e56d690e7579f7cc15d3d2daff5bea28208810'
+    checksumType64 = 'sha256'
+}
+# *** Automatically filled ***
+ 
+Install-ChocolateyZipPackage @packageArgs
+ 
+$logPath = Join-Path $Env:ChocolateyPackageFolder "eclipse.$packageVersion.txt"
+Set-Content $logPath $installationPath -Encoding UTF8 -Force
+ 
+$shortcutName = "Eclipse $packageVersion.lnk"
+$eclipsePath = Join-Path $installationPath 'eclipse\eclipse.exe'
+ 
+if ($multiUser) {
+    $shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonDesktopDirectory)) $shortcutName
+    $targetPath = Join-Path $installationPath 'eclipse.bat'
+    $targetContent = @"
+@echo off
+ 
+set ECLIPSEDIR="%LOCALAPPDATA%\Eclipse.$packageVersion"
+ 
+if not exist %ECLIPSEDIR% (
+  mkdir %ECLIPSEDIR%
+)
+ 
+start "Eclipse" "$eclipsePath" -configuration %ECLIPSEDIR%
+ 
+"@
+    Set-Content $targetPath $targetContent -Encoding Ascii -Force
+    Install-ChocolateyShortcut -ShortcutFilePath $shortcutPath -TargetPath $targetPath -IconLocation $eclipsePath
+} else {
+    $shortcutPath = Join-Path $([Environment]::GetFolderPath([System.Environment+SpecialFolder]::CommonPrograms)) $shortcutName
+    Install-ChocolateyShortcut -ShortcutFilePath $shortcutPath -TargetPath $eclipsePath -PinToTaskbar
+}
